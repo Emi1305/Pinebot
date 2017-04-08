730587f8665ae7c1f17a08f4e08792e662998861
Emi1305 <edelpeon@gmail.com>
2017-04-08 20:55:06 +0000

    Bot token now goes in a separated file. Added /help message.

--- pinebot.py
+++ pinebot.py
@@ -1,24 +1,35 @@
 import telebot
 from time import sleep
+import os 
 
+with open(os.getcwd() + 'token', 'r') as f:
+    token = f.read()
 
-token = <TOKEN> #gitignore Replace with your token
-
+log = os.getcwd() + '.pinebot_history'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start', 'help'])
+available_commands = (
+    '/start\t:\tStarts the bot',
+    '/help\t:\tShows this help message',
+    '/download_history\t:\tDownloads message history'
+)
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Buenas wachin')
-    with open('/home/deb/Documents/pinebot_messages', 'a+') as f:
+    bot.reply_to(message, 'Welcome to the pineapple telegram bot')
+    with open(log, 'a+') as f:
         f.write(message.text)
         f.write('\n')
 
-@bot.message_handler(commands=['archivo'])
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, '\n'.join(available_commands))
+
+@bot.message_handler(commands=['download_history'])
 def send_file(message):
-    with open('/home/deb/Documents/pinebot_messages', 'r+') as f:
+    with open(log, 'r+') as f:
         bot.send_document(message.chat.id, f)
         f.write(message.text)
-        f.write(help(message))
         f.write('\n')
 
 bot.polling()
